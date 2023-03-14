28d067bca42391c5c75b29f4f8e41fff18897f36
Andrei Visan <andrei.m.visan@gmail.com>
2023-03-14 17:45:22 +0100

    Created Page Service

--- src/main/java/io/programminglife/second_brain/model/notion/page/Note.java
+++ src/main/java/io/programminglife/second_brain/model/notion/page/Note.java
@@ -1,13 +1,11 @@
 package io.programminglife.second_brain.model.notion.page;
 
-import io.programminglife.second_brain.model.notion.block.Block;
+import lombok.Builder;
 
-import java.util.List;
-
+@Builder
 public class Note {
 
     private String title;
     private String url;
-    private List<Block> content;
 
 }
--- src/main/java/io/programminglife/second_brain/service/interfaces/notion/page/PageService.java
+++ src/main/java/io/programminglife/second_brain/service/interfaces/notion/page/PageService.java
@@ -1,9 +1,14 @@
 package io.programminglife.second_brain.service.interfaces.notion.page;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.programminglife.second_brain.model.notion.page.Note;
+
+import java.net.URISyntaxException;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PageService {
 
-    public abstract String getPage(UUID pageId);
+    public abstract Optional<Note> getPage(UUID pageId) throws URISyntaxException, JsonProcessingException;
 
 }
